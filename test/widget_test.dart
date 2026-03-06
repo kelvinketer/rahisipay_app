c4b9315772b561bb76b421fbef9d0fcb00bfaa4b
Kelvin Keter <33624862+kelvinketer@users.noreply.github.com>
2026-03-06 09:51:25 +0300

    feat: complete corporate rebranding to Rahisi Agro Pay and update Application ID

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rahisipay_app/main.dart';
+import 'package:rahisi_agro_pay/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
